4bc2ae1bcdc370aaf9238032b553461818ddf369
Gabriel Schaldach Morgado <72992857+schaldach@users.noreply.github.com>
2025-05-28 21:50:03 +0000

    variaveis modificaveis

--- head.h
+++ head.h
@@ -8,7 +8,18 @@
 // MODIFICÁVEIS
 // 16 ou 32
 #define logical_adress_bit_size 32
+
 #define hex true
+
+#define max_frame_number 100
+// o limite total seria aproximadamente 244 para 32 bits (páginas de 4KB e 1 milhão de linhas)
+// e 488 para 16 bits (páginas de 2KB)
+
+#define page_table_32b_number 1024
+#define page_table_32b_row_number 1024
+#define page_table_16b_row_number 32
+
+#define tlb_row_number 16
 
 // CONSTANTES
 // páginas de 2KB para 16 bits, 4KB para 32 bits
@@ -22,15 +33,6 @@
     #define input_format "%u\0"
 #endif
 
-#define max_frame_number 100
-// o limite total seria aproximadamente 244 para 32 bits (páginas de 4KB e 1 milhão de linhas)
-// e 488 para 16 bits (páginas de 2KB)
-
-#define page_table_32b_number 1024
-#define page_table_32b_row_number 1024
-#define page_table_16b_row_number 32
-
-#define tlb_row_number 16
 
 #define MEMORY_LINE_SIZE 20
 #define MEMORY_FILE "data_memory.txt"
